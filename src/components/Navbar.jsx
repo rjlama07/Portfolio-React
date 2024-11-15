2954f25527959c881d747df3c6842498aed68fcc
Ritesh Jung Lama <109976988+rjlama07@users.noreply.github.com>
2024-11-16 04:25:27 +0545

    added: linka and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,32 @@ const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="Kevin Rush Logo" />
+        <h1 className="mx-2 w-10 text-4xl">RJ</h1>
       </div>
       <div className="m-8 flex justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
+        <a
+          href="https://www.linkedin.com/in/ritesh-lama-b343b12b4/"
+          className="transform transition-transform duration-200 hover:scale-125"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/rjlama07"
+          className="transform transition-transform duration-200 hover:scale-125"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/its_rjlama/"
+          className="transform transition-transform duration-200 hover:scale-125"
+        >
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
+
+// transform transition-transform duration-200 hover:scale-125
