fedb87ff42b6663fd3a63ae908b313d650bc0b33
Ritesh Jung Lama <109976988+rjlama07@users.noreply.github.com>
2025-11-22 00:32:27 +1100

    Change title from 'Flutter Developer' to 'Mobile App Developer'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,7 +36,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent"
             >
-              Flutter Developer
+              Mobile App Developer
             </motion.span>
             <motion.p
               variants={container(1)}
